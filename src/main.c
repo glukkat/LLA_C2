28d796e0050369b87d09cc4d68e3754360a4cbaf
glukkat <glukkat@protonmail.com>
2026-03-25 01:12:50 +0100

    kv_put 2

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <assert.h>
+#include <stdio.h>
 
 #include "kv.h"
 
 int main() {
 	kv_t *db = kv_init(16);
 	kv_put(db, "Hi", "Hello");
+	for (int i = 0; i < db->capacity; i++) {
+		if (db->entries[i].key) printf("%d - %s: %s\n", i, db->entries[i].key, db->entries[i].value);
+	}
 }
